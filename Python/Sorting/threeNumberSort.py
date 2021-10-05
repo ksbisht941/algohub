a7e66ec146c6b21399d90cd2aeb2adfb1907f6b2
ksbisht <ksbisht941@gmail.com>
2021-10-06 00:04:43 +0530

    three number sort

--- Python/Sorting/threeNumberSort.py
+++ Python/Sorting/threeNumberSort.py
@@ -1,6 +1,24 @@
-def threeNumberSort(array, order):
-    # Write your code here.
-    pass
+# def threeNumberSort(array, order):
+#     valueCounts = [0, 0, 0]
+
+#     for element in array:
+#         orderIdx = order.index(element)
+#         valueCounts[orderIdx] += 1
+
+#     print(valueCounts)
+
+#     for idx in range(3):
+#         value = order[idx]
+#         count = valueCounts[idx]
+
+#         numElementsBefore = sum(valueCounts[:idx])
+#         print(numElementsBefore)
+
+#         for nth in range(count):
+#             currentIdx = numElementsBefore + nth
+#             array[currentIdx] = value
+
+#     return array
 
 
 array = [1, 0, 0, -1, -1, 0, 1, 1]
