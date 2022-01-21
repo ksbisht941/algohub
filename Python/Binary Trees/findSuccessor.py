e599116aaa9283fd229938aaa8ac08e0f6e793ef
ksbisht <ksbisht941@gmail.com>
2022-01-21 23:04:57 +0530

    .

--- Python/Binary Trees/findSuccessor.py
+++ Python/Binary Trees/findSuccessor.py
@@ -1,3 +1,12 @@
+# Find Successor
+# Write a function that takes in a Binary Tree (where nodes have an additional pointer to their parent node) as well as a node contained in that tree and returns the given node's successor.
+
+# A node's successor is the next node to be visited (immediately after the given node) when traversing its tree using the in-order tree-traversal technique. A node has no successor if it's the last node to be visited in the in-order traversal.
+
+# If a node has no successor, your function should return None / null.
+
+# Each BinaryTree node has an integer value, a parent node, a left child node, and a right child node. Children nodes can either be BinaryTree nodes themselves or None / null.
+
 # This is an input class. Do not edit.
 class BinaryTree:
     def __init__(self, value, left=None, right=None, parent=None):
@@ -49,7 +58,7 @@ def getLeftMostChild(node):
 def getRightMostParent(node):
     currentNode = node
 
-    while currentNode.parent is not None and currentNode.parenet.right == currentNode:
+    while currentNode.parent is not None and currentNode.parent.right == currentNode:
         currentNode = currentNode.parent
 
     return currentNode
