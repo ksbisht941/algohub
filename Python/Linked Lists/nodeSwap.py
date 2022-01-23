319f8b2baa5179e21b1aca3466d9bd8b829ab2a0
ksbisht <ksbisht941@gmail.com>
2022-01-23 15:49:52 +0530

    Node Swap

--- Python/Linked Lists/nodeSwap.py
+++ Python/Linked Lists/nodeSwap.py
@@ -14,15 +14,34 @@ class LinkedList:
         self.next = None
 
 
+# def nodeSwap(head):
+#     if head is None or head.next is None:
+#         return head
+
+#     nextNode = head.next
+#     head.next = nodeSwap(head.next.next)
+#     nextNode.next = head
+
+#     return nextNode
+
 def nodeSwap(head):
-    if head is None or head.next is None:
-        return head
+    tempNode = LinkedList(0)
+    tempNode.next = head
 
-    nextNode = head.next
-    head.next = nodeSwap(head.next.next)
-    nextNode.next = head
+    prevNode = tempNode
 
-    return nextNode
+    while prevNode.next is not None and prevNode.next.next is not None:
+        firstNode = prevNode.next
+        secondNode = prevNode.next.next
+
+        firstNode.next = secondNode.next
+        secondNode.next = firstNode
+        prevNode.next = secondNode
+
+        prevNode = firstNode
+
+    return tempNode.next
+
 
 # 0 -> 1 -> 2 -> 3 -> 4 -> 5
 # 1 -> 0 -> 3 -> 2 -> 5 -> 4
